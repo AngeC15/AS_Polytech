2184e41629f5ef1d5ac76d61de6ebd9ba6caed2e
Gaetan Lavirotte <gaetan.lavirotte@etu.univ-grenoble.fr>
2022-02-16 09:35:58 +0100

    Compilation successful but not tested

--- src/kernel/sys/sem/semop.c
+++ src/kernel/sys/sem/semop.c
@@ -1,5 +1,6 @@
 #include <nanvix/syscall.h>
 #include <errno.h>
+#include <sys/sem.h>
 
 
 
@@ -8,7 +9,7 @@ int sys_semop(int idSem, int op){
 
     pSemaphoreChart cell = getCell(idSem);
     //check if the cell has not a null semaphore and the semaphore is valid
-    if(cell == NULL || !cell.valide){
+    if(cell == NULL || !cell->valide){
         return -1;
     }
 
@@ -20,16 +21,13 @@ int sys_semop(int idSem, int op){
         res = down(idSem);
         enable_interrupts();
     }else if(op == 0){
-        //do nothing
+        return -1;
     }else if(op > 0){
         //Call up
         disable_interrupts();
         res = up(idSem);
         enable_interrupts();
     }
-
-
-
-
-    return rest;
+    
+    return res;
 }
