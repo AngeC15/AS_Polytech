60757c346752709d956799220fd26ce0a06b8bbd
Gaetan Lavirotte <gaetan.lavirotte@etu.univ-grenoble.fr>
2022-03-21 11:45:32 +0100

    Inifinite fork to saturate RAM

---
+++ src/ubin/crash/crash.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stropts.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/utsname.h>
+
+#include <nanvix/accounts.h>
+#include <nanvix/config.h>
+#include <dev/tty.h>
+
+/**
+ *  crash via "infinite fork"
+ */
+
+static void infinite_fork(void)
+{
+    while(1)
+	{
+        pid_t pid = fork(); // création de nouveau processus
+        printf("processus %d lance\n", pid); 
+    }
+}
+
+
+int main(int argc, char *const argv[])
+{
+    ((void)argc);
+	((void)argv);
+	infinite_fork();
+	return (EXIT_SUCCESS);
+}
